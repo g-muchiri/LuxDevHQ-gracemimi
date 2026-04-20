68d9701841fdbab5717d0de6a572b700d14122ce
g-muchiri <nmuchirig@gmail.com>
2026-04-20 22:52:08 +0300

    I have completed the case when and successfully completed assignment 1. I cant believe I can now use git and github

--- SQL-ass-and-class/assignment1.sql
+++ SQL-ass-and-class/assignment1.sql
@@ -152,14 +152,26 @@ select * from subjects where subject_name ilike '%studies%';
 
 /*
  * FINAL SECTION:- CASE WHEN
- * 
+ * Write a query using CASE WHEN to label each exam result with a grade description:
  * 'Distinction' if marks >= 80
  * 'Merit' if marks >= 60
  * 'Pass' if marks >= 40
  * 'Fail' if marks below 40
  *  Call the new column performance
  * 
+ */
+select result_id, student_id, subject_id, marks, grade, 
+case 
+	when marks >= 80 then 'Distinction'
+	when marks >=60 then 'Merit'
+	when marks >=40 then 'Pass'
+	else 'Fail'
+	end as performance
+from exam_results;
+
+/*
  * 
+ * Write a query using CASE WHEN to label each student as
  * •	'Senior' if they are in Form 3 or Form 4
  * •	'Junior' if they are in Form 2 or Form 1
  * Call the new column student_level
@@ -168,9 +180,17 @@ select * from subjects where subject_name ilike '%studies%';
  * 
  * 
  * */
+select * from students;
 
+select first_name, last_name, gender,"class",
+case
+	when class in ('Form 3','Form-3', 'Form 4', 'Form-4' ) then 'Senior'
+	when class in ('Form 2','Form-2', 'Form 1', 'Form-1') then 'Junior'
+end as student_level
+from students;
 
-
+--Practising update
+update students set class = 'Form 3' where student_id = 3;
 
 
 
