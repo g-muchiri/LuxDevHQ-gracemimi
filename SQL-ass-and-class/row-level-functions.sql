7836073ed7c8192e106f56ee5626eb4c1f9eb7b8
g-muchiri <nmuchirig@gmail.com>
2026-04-21 22:19:23 +0300

    This marks the end of my row level class with date and time functions

--- SQL-ass-and-class/row-level-functions.sql
+++ SQL-ass-and-class/row-level-functions.sql
@@ -70,14 +70,64 @@ select 26.9876 as our_number, round(26.9876,2) as rounded_to_two,
 ceil(26.9876) as ceiling, floor(26.9876) as floored;
 
 --aggregate functions
-select student_id, count(*) as number_of_exams
+select student_id, count(*) as number_of_exams, avg(marks) as avg_marks
 from exam_results
 group by student_id
 having count(*) > 1;
 
+select*from subjects;
+
+select count(*) from subjects where department = 'Sciences';
 
 
+/*
+ * Date and time functions
+ * - YEAR()
+ * - MONTH()
+ * - DAY()
+ * - DATEDIFF()
+ * - DATE_TRUNC('year', date '2026/04/21')
+ * - INTERVAL()
+ * - CURRENT_DATE() gives you a timestamp of the current date
+ * - DATEADD(month, number of months to add, 'date')
+ * - TO_CHAR() converts into human friendly format	
+ * */
+--using extract
+select first_name, last_name, extract(year from age(now(), date_of_birth))as age_of_student from students;
 
+-- using date_trunc
+select first_name, last_name, date_trunc('year', age(now(), date_of_birth))as age_of_student from students;
+--date trunc gives you theprefix 18years or 12months etc
+
+--the two functions above serve the same function. Getting the years of the student
+
+--the most important thing to know is this, if you are writting your date as a string, 
+-- you will need to put it into single quote and cast it into a date in order to use time functions
+
+--casting into a date 
+
+select date '1992-09-02';
+
+select DATE_TRUNC('year', date '2026/04/21');
+
+/*using intervals
+ * 
+ * the function returns a date
+ * 
+ * now() - interval '30 days' means 30 days ago
+ * now() - inteval '60 days' means 60 days ago
+*/
+
+select now()::date - interval '26 years';
+
+--given that an exams release date is 14 days later, 
+--please show the student id, exam id, exam date and release date
+
+select result_id, student_id, exam_date, 
+(exam_date + interval '14 days')::date as release_date from exam_results;
+
+
+select to_char(exam_date,'Day, dd month yyyy') from exam_results;
 
 
 
